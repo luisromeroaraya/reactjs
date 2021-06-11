ed25f783b46c2beb2ac88f68168beda7e8c8c3b7
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-06-11 17:51:46 +0200

    Add running clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,6 +1,13 @@
-let time = new Date().toLocaleTimeString();
+import { useState } from 'react';
 
 function Clock() {
+  let time = new Date().toLocaleTimeString();
+  const [Ctime, setCtime] = useState(time);
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  }
+  setInterval(updateTime, 1000);
   return <p>The time is {time}</p>;
 }
 
